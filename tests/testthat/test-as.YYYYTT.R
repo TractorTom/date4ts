f6ca5eab5eabf761390afd1552ae456140e20b7c
TractorTom <tangbarth@hotmail.fr>
2023-10-23 21:25:58 +0200

    renv status ??

---
+++ tests/testthat/test-as.YYYYTT.R
@@ -0,0 +1,28 @@
+# Initialisation ---------------------------------------------------------------
+
+set.seed(2027L)
+
+
+# Tests de résultats positifs --------------------------------------------------
+
+testthat::test_that("good result for integer date", {
+    for (quarter in c(warning_integer_quarters, good_quarters)) {
+        for (year in good_years) {
+            TSdate <- as.integer(c(year + quarter %/% 4L, quarter %% 4L + 1L))
+            res <- as.YYYYTT(year + quarter / 4L)
+            testthat::expect_type(res, "integer")
+            testthat::expect_identical(res, TSdate)
+        }
+    }
+})
+
+# Tests de résultats négatifs --------------------------------------------------
+
+testthat::test_that("miscellaneous input are not allowed", {
+    for (wrong_time in object_bank_R[-c(10L, 16L)]) {
+        testthat::expect_error(as.YYYYTT(wrong_time))
+    }
+    for (wrong_time in wrong_TimeUnits) {
+        testthat::expect_error(as.YYYYTT(wrong_time))
+    }
+})
---
+++ tests/testthat/test-assert_date_ts.R
@@ -0,0 +1,324 @@
+# Initialisation ---------------------------------------------------------------
+
+set.seed(2024L)
+
+
+# Tests de résultats positifs --------------------------------------------------
+
+testthat::test_that("good result for integer date month", {
+    for (year in good_years) {
+        testthat::expect_silent(assert_date_ts(year, frequency_ts = 12L))
+        for (month in good_months) {
+            testthat::expect_silent(
+                assert_date_ts(c(year, month), frequency_ts = 12L)
+            )
+        }
+    }
+})
+
+testthat::test_that("good result for integer date quarter", {
+    for (year in good_years) {
+        testthat::expect_silent(assert_date_ts(year, frequency_ts = 4L))
+        for (quarter in good_quarters) {
+            testthat::expect_silent(
+                assert_date_ts(c(year, quarter), frequency_ts = 4L)
+            )
+        }
+    }
+})
+
+
+# Tests positifs avec warning --------------------------------------------------
+
+## Mensuel -----------------------------------------------------------------
+
+testthat::test_that("warning for integer date", {
+    for (good_year in good_years) {
+        for (warning_month in warning_integer_months) {
+            warning_date <- c(good_year, warning_month)
+            testthat::expect_warning(
+                assert_date_ts(warning_date, frequency_ts = 12L),
+                regexp = invalid_monthly_period
+            )
+        }
+    }
+})
+
+testthat::test_that("warning for double date", {
+    for (warning_year in double_years) {
+        warning_date <- warning_year
+        testthat::expect_warning(
+            assert_date_ts(warning_date, frequency_ts = 12L),
+            regexp = double_instead_of_integer
+        )
+
+        for (good_month in good_months) {
+            warning_date <- c(warning_year, good_month)
+            testthat::expect_warning(
+                assert_date_ts(warning_date, frequency_ts = 12L),
+                regexp = double_instead_of_integer
+            )
+        }
+    }
+
+    for (good_year in good_years) {
+        for (warning_month in double_months) {
+            warning_date <- c(good_year, warning_month)
+            testthat::expect_warning(
+                assert_date_ts(warning_date, frequency_ts = 12L),
+                regexp = double_instead_of_integer
+            )
+        }
+    }
+
+    for (warning_year in double_years) {
+        for (warning_month in double_months) {
+            warning_date <- c(warning_year, warning_month)
+            testthat::expect_warning(
+                assert_date_ts(warning_date, frequency_ts = 12L),
+                regexp = double_instead_of_integer
+            )
+        }
+    }
+})
+
+testthat::test_that("several warning", {
+    for (warning_year in double_years) {
+        for (warning_month in warning_integer_months) {
+            warning_date <- c(warning_year, warning_month)
+            w <- testthat::capture_warnings(
+                assert_date_ts(warning_date, frequency_ts = 12L)
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_monthly_period,
+                all = FALSE
+            )
+        }
+    }
+
+    for (warning_year in double_years) {
+        for (warning_month in warning_double_months) {
+            warning_date <- c(warning_year, warning_month)
+            w <- testthat::capture_warnings(
+                assert_date_ts(warning_date, frequency_ts = 12L)
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_monthly_period,
+                all = FALSE
+            )
+        }
+    }
+
+    for (good_year in good_years) {
+        for (warning_month in warning_double_months) {
+            warning_date <- c(warning_year, warning_month)
+            w <- testthat::capture_warnings(
+                assert_date_ts(warning_date, frequency_ts = 12L)
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_monthly_period,
+                all = FALSE
+            )
+        }
+    }
+})
+
+
+## Trimestriel -----------------------------------------------------------------
+
+testthat::test_that("warning for integer date", {
+    for (good_year in good_years) {
+        for (warning_quarter in warning_integer_quarters) {
+            warning_date <- c(good_year, warning_quarter)
+            testthat::expect_warning(
+                {
+                    boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+                },
+                regexp = invalid_quaterly_period
+            )
+            testthat::expect_silent(boolRes)
+        }
+    }
+})
+
+testthat::test_that("warning for double date", {
+    for (warning_year in double_years) {
+        warning_date <- warning_year
+        testthat::expect_warning(
+            {
+                boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+            },
+            regexp = double_instead_of_integer
+        )
+        testthat::expect_silent(boolRes)
+
+        for (good_quarter in good_quarters) {
+            warning_date <- c(warning_year, good_quarter)
+            testthat::expect_warning(
+                {
+                    boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+                },
+                regexp = double_instead_of_integer
+            )
+            testthat::expect_silent(boolRes)
+        }
+    }
+
+    for (good_year in good_years) {
+        for (warning_quarter in double_quarters) {
+            warning_date <- c(good_year, warning_quarter)
+            testthat::expect_warning(
+                {
+                    boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+                },
+                regexp = double_instead_of_integer
+            )
+            testthat::expect_silent(boolRes)
+        }
+    }
+
+    for (warning_year in double_years) {
+        for (warning_quarter in double_quarters) {
+            warning_date <- c(warning_year, warning_quarter)
+            testthat::expect_warning(
+                {
+                    boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+                },
+                regexp = double_instead_of_integer
+            )
+            testthat::expect_silent(boolRes)
+        }
+    }
+})
+
+testthat::test_that("several warning", {
+    for (warning_year in double_years) {
+        for (warning_quarter in warning_integer_quarters) {
+            warning_date <- c(warning_year, warning_quarter)
+            w <- testthat::capture_warnings({
+                boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+            })
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_quaterly_period,
+                all = FALSE
+            )
+
+            testthat::expect_silent(boolRes)
+        }
+    }
+
+    for (warning_year in double_years) {
+        for (warning_quarter in warning_double_quarters) {
+            warning_date <- c(warning_year, warning_quarter)
+            w <- testthat::capture_warnings({
+                boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+            })
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_quaterly_period,
+                all = FALSE
+            )
+
+            testthat::expect_silent(boolRes)
+        }
+    }
+
+    for (good_year in good_years) {
+        for (warning_quarter in warning_double_quarters) {
+            warning_date <- c(warning_year, warning_quarter)
+            w <- testthat::capture_warnings({
+                boolRes <- assert_date_ts(warning_date, frequency_ts = 4L)
+            })
+            testthat::expect_match(
+                object = w,
+                regexp = double_instead_of_integer,
+                all = FALSE
+            )
+            testthat::expect_match(
+                object = w,
+                regexp = invalid_quaterly_period,
+                all = FALSE
+            )
+
+            testthat::expect_silent(boolRes)
+        }
+    }
+})
+
+
+## Fréquence -------------------------------------------------------------------
+
+testthat::test_that("warning for integer date", {
+    for (good_year in good_years) {
+        for (good_quarter in good_quarters) {
+            good_date <- c(good_year, good_quarter)
+            testthat::expect_warning(
+                {
+                    boolRes <- assert_date_ts(good_date, frequency_ts = 4.)
+                },
+                regexp = warning_frequency_double
+            )
+            testthat::expect_silent(boolRes)
+        }
+    }
+
+    for (good_year in good_years) {
+        for (good_month in good_months) {
+            good_date <- c(good_year, good_month)
+            testthat::expect_warning(
+                assert_date_ts(good_date, frequency_ts = 12.),
+                regexp = warning_frequency_double
+            )
+        }
+    }
+})
+
+
+# Tests de résultats négatifs --------------------------------------------------
+
+testthat::test_that("detection of wrong dates", {
+    for (wrong_date in c(object_bank_R[-10L], wrong_dates)) {
+        testthat::expect_error(assert_date_ts(wrong_date, frequency_ts = 12L))
+        testthat::expect_error(assert_date_ts(wrong_date, frequency_ts = 4L))
+    }
+})
+
+testthat::test_that("miscellaneous frequency are not allowed", {
+    for (wrong_frequency in c(object_bank_R, weird_frequency)) {
+        testthat::expect_error(
+            assert_date_ts(
+                date_ts = create_random_date(),
+                frequency_ts = wrong_frequency
+            )
+        )
+    }
+})
