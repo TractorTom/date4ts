438e42e32613b046428ae30b5313878840d1fa9e
TractorTom <tangbarth@hotmail.fr>
2024-09-12 14:18:47 +0200

    new release v1.0.1

--- tests/testthat/test-check-assert_date_ts.R
+++ tests/testthat/test-check-assert_date_ts.R
@@ -1,6 +1,3 @@
-# Initialisation ---------------------------------------------------------------
-
-set.seed(2024L)
 
 assert_date_ts(x = 2020L, frequency_ts = 12L)
 assert_date_ts(x = c(2020L, 12L), frequency_ts = 12L)
